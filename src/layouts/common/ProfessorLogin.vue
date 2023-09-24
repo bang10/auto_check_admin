2841a133e91d3ef8978785c48d8024e128989d18
bang10 <bang369953@gmail.com>
2023-09-24 09:51:51 +0900

    init commit

---
+++ src/layouts/common/ProfessorLogin.vue
@@ -0,0 +1,44 @@
+<script setup>
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+
+const professorId = ref('')
+const professorName = ref('')
+
+const router = useRouter()
+const fnLogin = () => {
+  console.log(professorId.value, professorName.value)
+  router.push({ name: 'Main' })
+}
+</script>
+
+<template>
+    <div class="q-pa-md" style="max-width: 400px">
+
+      <q-form
+        @submit="fnLogin"
+        class="q-gutter-md"
+      >
+        <q-input
+          filled
+          v-model="professorId"
+          label="교수번호"
+          hint="교수 번호를 입력해 주세요."
+          lazy-rules
+        />
+
+        <q-input
+          filled
+          v-model="professorName"
+          label="이름"
+          hint="이름를 입력해 주세요."
+          lazy-rules
+        />
+
+        <div>
+          <q-btn label="로그인" type="submit" color="primary"/>
+        </div>
+      </q-form>
+
+    </div>
+</template>
---
+++ src/router/routes.js
@@ -0,0 +1,24 @@
+const routes = [
+  {
+    path: '/main',
+    name: 'Main',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [
+      { path: '', component: () => import('pages/IndexPage.vue') }
+    ]
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: () => import('layouts/common/ProfessorLogin.vue')
+  }
+
+  // Always leave this as last one,
+  // but you can also remove it
+  // {
+  //   path: '/:catchAll(.*)*',
+  //   component: () => import('pages/ErrorNotFound.vue')
+  // }
+]
+
+export default routes
