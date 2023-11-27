db11cbf47b5662d398c04aa1c0742c883594f95f
bangseonghwan <bang369953@gmail.com>
2023-11-28 01:16:09 +0900

    통계 시간 관련 수정

--- src/layouts/AttendanceManage.vue
+++ src/layouts/AttendanceManage.vue
@@ -106,6 +106,7 @@ const getAttendance = async () => {
 
 const fnTranceTime = (param) => {
   const date = new Date(param)
+  date.setHours(date.getHours() - 9)
   const year = date.getFullYear()
   const month = String(date.getMonth() + 1).padStart(2, '0')
   const day = String(date.getDate()).padStart(2, '0')
